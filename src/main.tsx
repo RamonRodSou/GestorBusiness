7038521c33273dd700f5c5ab728c5d4ba0a5c96f
Ramon Rodrigues <ramonrodsouza@gmail.com>
2025-04-21 01:55:35 -0300

    feature-001- feat: criando projeto React Gestor

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './reset.css'
 import './index.css'
 import AppRouter from './AppRouter'
-import { Container } from '@mui/material'
 
 createRoot(document.getElementById('root')!).render(
-    <Container>
-        <StrictMode>
-            <AppRouter/>
-        </StrictMode>
-    </Container>
+    <StrictMode>
+        <AppRouter/>
+    </StrictMode>
 )
