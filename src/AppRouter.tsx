5ba0279a93ab13007992ab3ca7faf6885db1055f
Ramon Rodrigues <ramonrodsouza@gmail.com>
2025-04-21 02:00:56 -0300

    feature-004 - feat: Criando rotas

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -3,21 +3,25 @@ import Login from '@components/login/Login';
 import Dashboard from '@components/dashboard/Dashboard';
 import { JSX } from '@emotion/react/jsx-runtime';
 import { AuthService } from '@service/auth-service';
+import Home from '@components/dashboard/pages/home/Home';
+import Client from '@components/dashboard/pages/client/Client';
+import Financial from '@components/dashboard/pages/financial/Financial';
+import ServiceOrder from '@components/dashboard/pages/service-order/ServiceOrder';
+import Preferences from '@components/dashboard/pages/preferences/Preferences';
 
 function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route 
-                    path="/dashboard/:userId" 
-                    element={
-                        <ProtectedRoute>
-                            <Dashboard />
-                        </ProtectedRoute>
-                    } 
-                />
-                <Route path="*" element={<Navigate to="/login" />} />
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/dashboard/:userId" element={<ProtectedRoute><Dashboard/></ProtectedRoute>}>
+                    <Route path="home" element={<Home/>}/>
+                    <Route path="clients" element={<Client/>}/>
+                    <Route path="financial" element={<Financial/>}/>
+                    <Route path="service-order" element={<ServiceOrder/>}/>
+                    <Route path="preferences" element={<Preferences/>}/>
+                </Route>
+                <Route path="*" element={<Navigate to="/login"/>}/>
             </Routes>
         </BrowserRouter>
     );
@@ -27,7 +31,6 @@ function ProtectedRoute({ children }: { children: JSX.Element }) {
     if (!AuthService.isAuthenticated()) {
         return <Navigate to="/login" replace />;
     }
-
     return children;
 }
 
