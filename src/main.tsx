5960a7b48f140d87a184cf5721e7add9a09225f5
Ramon Rodrigues <ramonrodsouza@gmail.com>
2025-04-22 02:02:03 -0300

    feature-009 - refactor: colocando os providers main.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './reset.css'
-import './index.css'
-import AppRouter from './AppRouter'
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './reset.css';
+import './index.css';
+import AppRouter from './AppRouter';
+import { AuthProvider } from './context/AuthContext';
+import { ManagerProvider } from '@context/ManagerContext';
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <AppRouter/>
-    </StrictMode>
+    <React.StrictMode>
+        <AuthProvider>
+            <ManagerProvider>
+                <StrictMode>
+                    <AppRouter/>
+                </StrictMode>
+            </ManagerProvider>
+        </AuthProvider>
+    </React.StrictMode>
 )
