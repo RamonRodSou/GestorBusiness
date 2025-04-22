7e1e96cf0a2f9aba0fc672896c742c07ccf26307
Ramon Rodrigues <ramonrodsouza@gmail.com>
2025-04-22 18:37:17 -0300

    feature-016 - refactor: add barra de pesquisa de cliente client. styles  mudando nome de classes de new cliente

--- src/components/dashboard/pages/client-data/Client-data.tsx
+++ src/components/dashboard/pages/client-data/Client-data.tsx
@@ -1,73 +1,73 @@
 import './client-data.scss';
 import { Add } from "@mui/icons-material";
-import { Box, Container, IconButton, Tooltip, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  IconButton,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import { useContext, useEffect, useState } from 'react';
 import { Client } from '@domain/user/Client';
 import { findAllClients } from '@service/UserService';
 import { ManagerContext } from '@context/ManagerContext';
 import { useNavigate, useParams } from 'react-router-dom';
+import Search from '@components/search/Search';
 
 export default function ClientData() {
-    const [data, setData] = useState<Client[]>();
+    const [data, setData] = useState<Client[]>([]);
+    const [filtered, setFiltered] = useState<Client[]>([]);
     const { userId } = useParams();
     const { isMobile } = useContext(ManagerContext);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        findAllClients()
+            .then((res) => {
+                setData(res);
+                setFiltered(res);
+            })
+            .catch(console.error);
+    }, []);
 
     function newClient() {
         return navigate(`/dashboard/${userId}/new-client`);
     }
 
-    useEffect(() => {
-        findAllClients()
-            .then(setData)
-            .catch(console.error);
-    }, []);
-
     return (
-        <Container className='container'>
-            {data?.map((it) => (
-                <Box className='list' key={it.id}>
-                    <Typography className='text'>
-                        {it?.name}
-                    </Typography>
-                    <Typography className='text'>
-                        {it?.phone}
-                    </Typography>
-                    {isMobile 
-                        ? <></> 
-                        : <>
-                            <Typography className='text'>
-                                {it?.street}
-                            </Typography>
-                            <Typography className='text'>
-                                {it?.houserNumber}
-                            </Typography>
-                            <Typography className='text'>
-                                {it?.city}
-                            </Typography>
-                            <Typography className='text'>
-                                {it?.state}
-                            </Typography>
-                           <Typography className='text'>
-                                {it?.houserNumber}
-                            </Typography>
-                            <Typography className='text'>
-                                {it?.zipCode}
-                            </Typography>
-                        </>
-                    }
-                    <Typography className='text'>
-                        {it?.status}
-                    </Typography>
+        <Container className='data-container'>
+            <Search<Client> data={data} onFilter={setFiltered} />
 
-                </Box>
-            ))}
+            {filtered?.length > 0 ? (
+                filtered.map((it) => (
+                    <Box className='data-list' key={it.id}>
+                        <Typography className='data-text'>{it?.name}</Typography>
+                        <Typography className='data-text'>{it?.phone}</Typography>
 
-            <Tooltip className='button' title="Click to new client">
+                        {!isMobile && (
+                            <>
+                                <Typography className='data-text'>{it?.street}</Typography>
+                                <Typography className='data-text'>{it?.houserNumber}</Typography>
+                                <Typography className='data-text'>{it?.city}</Typography>
+                                <Typography className='data-text'>{it?.state}</Typography>
+                                <Typography className='data-text'>{it?.zipCode}</Typography>
+                            </>
+                        )}
+
+                        <Typography className='data-text'>{it?.status}</Typography>
+                    </Box>
+                ))
+            ) : (
+                <Typography variant="body1" sx={{ color: 'var(--primary-title)' }}>
+                    Nenhum cliente encontrado.
+                </Typography>
+            )}
+
+            <Tooltip className='data-button' title="Click to new client">
                 <IconButton onClick={() => newClient()}>
-                    <Add/>
+                <Add />
                 </IconButton>
             </Tooltip>
         </Container>
-    )
+    );
 }
--- src/components/dashboard/pages/client-data/client-details/ClientDetails.tsx
+++ src/components/dashboard/pages/client-data/client-details/ClientDetails.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Box, Button, Container, TextField } from "@mui/material";
 import { Client } from "@domain/user/Client";
 import { clientAdd } from '@service/UserService';
+import BackButton from '@components/back-button/BackButton';
 
 export default function ClientDetails() {
     const [client, setClient] = useState<Client>(new Client());
@@ -16,100 +17,102 @@ export default function ClientDetails() {
         const newClient = { ...client, createdAt: new Date() };
         await clientAdd(newClient);
         setClient(new Client());
-        alert('Cliente registrado com sucesso!');
     };
 
     return (
-        <Container className='container'>
-            <form onSubmit={handleSubmit} className="form">
-                <h2>Novo Cliente</h2>
-                <Box mb={2}>
-                    <TextField
-                        label="Nome"
-                        value={client.name}
-                        onChange={(e) => handleChange("name", e.target.value)}
-                        fullWidth
-                        required
-                    />
-                </Box>
-                <Box mb={2}>
-                    <TextField
-                        label="Telefone"
-                        value={client.phone}
-                        onChange={(e) => handleChange("phone", e.target.value)}
-                        fullWidth
-                        required
-                    />
-                </Box>
-                <Box mb={2}>
-                    <TextField
-                        label="Email"
-                        type="email"
-                        value={client.email}
-                        onChange={(e) => handleChange("email", e.target.value)}
-                        fullWidth
-                        required
-                    />
-                </Box>
-                <Box mb={2}>
-                    <TextField
-                        label="Rua"
-                        value={client.street}
-                        onChange={(e) => handleChange("street", e.target.value)}
-                        fullWidth
-                        required
-                    />
-                </Box>
-                <Box mb={2}>
-                    <TextField
-                        label="Número"
-                        type="number"
-                        value={client.houserNumber ?? ""}
-                        onChange={(e) => handleChange("houserNumber", Number(e.target.value))}
-                        fullWidth
-                        required
-                    />
-                </Box>
-                <Box mb={2}>
-                    <TextField
-                        label="Cidade"
-                        value={client.city}
-                        onChange={(e) => handleChange("city", e.target.value)}
-                        fullWidth
-                        required
-                    />
-                </Box>
-                <Box mb={2}>
-                    <TextField
-                        label="Estado"
-                        value={client.state}
-                        onChange={(e) => handleChange("state", e.target.value)}
-                        fullWidth
-                        required
-                    />
-                </Box>
-                <Box mb={2}>
-                    <TextField
-                        label="CEP"
-                        value={client.zipCode}
-                        onChange={(e) => handleChange("zipCode", e.target.value)}
-                        fullWidth
-                        required
-                    />
-                </Box>
-                <Box mb={2}>
-                    <TextField
-                        label="País"
-                        value={client.country}
-                        onChange={(e) => handleChange("country", e.target.value)}
-                        fullWidth
-                        required
-                    />
-                </Box>
-                <Button type="submit" variant="contained" color="primary" fullWidth>
-                    Salvar Cliente
-                </Button>
-            </form>
-        </Container>
+        <>
+            <BackButton isToHome={false}/>
+            <Container className='details-container'>
+                <form onSubmit={handleSubmit} className="details-form">
+                    <h2>Novo Cliente</h2>
+                    <Box mb={2}>
+                        <TextField
+                            label="Nome"
+                            value={client.name}
+                            onChange={(e) => handleChange("name", e.target.value)}
+                            fullWidth
+                            required
+                        />
+                    </Box>
+                    <Box mb={2}>
+                        <TextField
+                            label="Telefone"
+                            value={client.phone}
+                            onChange={(e) => handleChange("phone", e.target.value)}
+                            fullWidth
+                            required
+                        />
+                    </Box>
+                    <Box mb={2}>
+                        <TextField
+                            label="Email"
+                            type="email"
+                            value={client.email}
+                            onChange={(e) => handleChange("email", e.target.value)}
+                            fullWidth
+                            required
+                        />
+                    </Box>
+                    <Box mb={2}>
+                        <TextField
+                            label="Rua"
+                            value={client.street}
+                            onChange={(e) => handleChange("street", e.target.value)}
+                            fullWidth
+                            required
+                        />
+                    </Box>
+                    <Box mb={2}>
+                        <TextField
+                            label="Número"
+                            type="number"
+                            value={client.houserNumber ?? ""}
+                            onChange={(e) => handleChange("houserNumber", Number(e.target.value))}
+                            fullWidth
+                            required
+                        />
+                    </Box>
+                    <Box mb={2}>
+                        <TextField
+                            label="Cidade"
+                            value={client.city}
+                            onChange={(e) => handleChange("city", e.target.value)}
+                            fullWidth
+                            required
+                        />
+                    </Box>
+                    <Box mb={2}>
+                        <TextField
+                            label="Estado"
+                            value={client.state}
+                            onChange={(e) => handleChange("state", e.target.value)}
+                            fullWidth
+                            required
+                        />
+                    </Box>
+                    <Box mb={2}>
+                        <TextField
+                            label="CEP"
+                            value={client.zipCode}
+                            onChange={(e) => handleChange("zipCode", e.target.value)}
+                            fullWidth
+                            required
+                        />
+                    </Box>
+                    <Box mb={2}>
+                        <TextField
+                            label="País"
+                            value={client.country}
+                            onChange={(e) => handleChange("country", e.target.value)}
+                            fullWidth
+                            required
+                        />
+                    </Box>
+                    <Button type="submit" variant="contained" color="primary" fullWidth>
+                        Salvar Cliente
+                    </Button>
+                </form>
+            </Container>
+        </>
     );
 }
