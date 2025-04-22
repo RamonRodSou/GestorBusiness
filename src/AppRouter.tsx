6c137872e29be652eeb180fa01eff263da2219df
Ramon Rodrigues <ramonrodsouza@gmail.com>
2025-04-22 02:04:12 -0300

    feature-010 - refactor: criando nova rota para new client

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -4,10 +4,11 @@ import Dashboard from '@components/dashboard/Dashboard';
 import { JSX } from '@emotion/react/jsx-runtime';
 import { AuthService } from '@service/auth-service';
 import Home from '@components/dashboard/pages/home/Home';
-import Client from '@components/dashboard/pages/client/Client';
 import Financial from '@components/dashboard/pages/financial/Financial';
 import ServiceOrder from '@components/dashboard/pages/service-order/ServiceOrder';
 import Preferences from '@components/dashboard/pages/preferences/Preferences';
+import ClientDetails from '@components/dashboard/pages/client-data/client-details/ClientDetails';
+import ClientData from '@components/dashboard/pages/client-data/Client-data';
 
 function AppRouter() {
     return (
@@ -16,7 +17,8 @@ function AppRouter() {
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/dashboard/:userId" element={<ProtectedRoute><Dashboard/></ProtectedRoute>}>
                     <Route path="home" element={<Home/>}/>
-                    <Route path="clients" element={<Client/>}/>
+                    <Route path="clients" element={<ClientData/>}/>
+                    <Route path="new-client" element={<ClientDetails/>}/>
                     <Route path="financial" element={<Financial/>}/>
                     <Route path="service-order" element={<ServiceOrder/>}/>
                     <Route path="preferences" element={<Preferences/>}/>
