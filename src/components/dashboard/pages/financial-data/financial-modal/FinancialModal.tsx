a005fb676bf031a913bc73c60b6ffdf08de83cb1
Ramon Rodrigues <ramonrodsouza@gmail.com>
2025-04-24 19:31:23 -0300

    feature-051 - feat: criando modal para financial

--- src/components/dashboard/pages/financial-data/financial-modal/FinancialModal.tsx
+++ src/components/dashboard/pages/financial-data/financial-modal/FinancialModal.tsx
@@ -1,12 +1,14 @@
+import { ServiceOrder } from '@domain/service-order';
 import './financial-modal.scss';
-import { Dialog, DialogTitle, DialogContent, DialogActions, TextField, Button } from '@mui/material';
+import { Dialog, DialogContent, DialogActions, TextField, Button, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 
-export default function FinancialModal({ open, onClose, onConfirm, incomeDefault }: {
+export default function FinancialModal({ open, onClose, onConfirm, incomeDefault, order }: {
     open: boolean;
     onClose: () => void;
     onConfirm: (income: number, expense: number) => void;
     incomeDefault: number;
+    order: ServiceOrder
 }) {
     const [income, setIncome] = useState(incomeDefault);
     const [expense, setExpense] = useState(0);
@@ -18,7 +20,10 @@ export default function FinancialModal({ open, onClose, onConfirm, incomeDefault
 
     return (
         <Dialog className='dialog-box' open={open} onClose={onClose}>
-            <DialogTitle className='title'>Finalizar Ordem de Serviço</DialogTitle>
+            <div className='dialog-header'>
+                <Typography className='dialog-title'>Finalizar Ordem de Serviço</Typography>
+                <Typography variant="h6" className='title-number'>Ordem Nº {order.orderNumber}</Typography>
+            </div>
             <DialogContent className="dialog-content">
                 <TextField
                     className="input-field"
