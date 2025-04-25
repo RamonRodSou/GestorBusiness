61170a4d01be6d649b665c2a2eb52be00bcd324e
Ramon Rodrigues <ramonrodsouza@gmail.com>
2025-04-25 02:46:33 -0300

    feature-059 - feat: criando hook para navegacao no dashboard

--- src/components/back-button/BackButton.tsx
+++ src/components/back-button/BackButton.tsx
@@ -1,30 +1,18 @@
+import { useDashboardNavigation } from '@hooks/useDashboardNavigation';
 import './back-button.scss'
-import { Admin } from '@domain/user';
 import { ArrowBack } from "@mui/icons-material";
 import { IconButton, Tooltip } from "@mui/material";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 type props = {
     path?:  string;
 }
 
 export default function BackButton({path}: props) {
-    const { userId } = useParams();
-    const location = useLocation();
-    const admin = location.state?.admin as Admin;
-    const navigate = useNavigate();
-
-    function nav (path: string) {
-        navigate(`/dashboard/${userId}/${path}`, { state: { admin } })
-    }
-
-    const backOrNavigate = () => {
-        nav( path ?? 'home')
-    }
+    const { goToDashboardPath } = useDashboardNavigation();    
 
     return (
         <Tooltip title="Click to back page" className='back-button' >
-            <IconButton onClick={() => backOrNavigate()}>
+            <IconButton onClick={() => goToDashboardPath( path ?? 'home')}>
                 <ArrowBack/>
             </IconButton>
         </Tooltip>
